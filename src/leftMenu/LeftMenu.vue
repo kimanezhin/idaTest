14970cd1a8e1ff10790cdf2cb38bfe9778c6fe4a
Kostya <kimanezhin@edu.hse.ru>
2019-06-08 19:49:02 +0300

    Adpativity on payment page

--- src/leftMenu/LeftMenu.vue
+++ src/leftMenu/LeftMenu.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="menu">
+    <!-- <div class="link">
+      <span>Личный кабинет</span>
+    </div>
+
+    <div @click="changeTab('link0')" class="link">
+      <span name="link0" :class="{current_link:isThisLink(0)}">Платежи</span>
+    </div>
+
+    <div @click="changeTab('link1')" class="link">
+      <span name="link1" :class="{current_link:isThisLink(1)}">История платежей</span>
+    </div>
+
     <div class="link">
+      <span>Настройки</span>
+    </div>
+
+    <div class="link">
+      <span class="current_link">Выйти</span>
+    </div> -->
+
+    <input type="checkbox" id="hmt" class="hidden-menu-ticker">
+    <!-- Далее вёрстка самой иконки. Разделена на спаны, что бы красиво превращать в крестик при открытии (почти как в Телеграме :) -->
+    <label class="btn-menu" for="hmt">
+      <span class="first"></span>
+      <span class="second"></span>
+      <span class="third"></span>
+    </label>
+    <!-- Менюшка раскрывающаяся и скрывающаяся -->
+    <ul class="hidden-menu">
+      <div class="link">
       <span>Личный кабинет</span>
     </div>
 
@@ -19,6 +48,7 @@
     <div class="link">
       <span class="current_link">Выйти</span>
     </div>
+    </ul>
   </div>
 </template>
 <script>
@@ -67,17 +97,5 @@ export default {
 
 
 <style>
-a {
-  width: 100%;
-  height: 100%;
-}
-
-@media (max-width:1000px) {
-  .menu{
-    display: none;
-  }
-}
-
-
 @import "../styles/leftMenu.css";
 </style>
